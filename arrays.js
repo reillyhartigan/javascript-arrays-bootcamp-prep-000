04db418524e1712c69e89576c0e6b0be9311fdf7
sensible-state-8093 <reilly.hartigan@yale.edu>
2019-02-16 20:18:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 return array.unshift(element)
 };
 function addElementToEndOfArray(array, element) {
-  return [array, ...element]
+  return [...array, element]
 };
 function destructivelyAddElementToEndOfArray(array, element) {
   return array.push(element)
