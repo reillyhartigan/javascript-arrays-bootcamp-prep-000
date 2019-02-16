e23f1b514820e9d7c11305d0f5b3b3f4e632ebd4
sensible-state-8093 <reilly.hartigan@yale.edu>
2019-02-16 20:11:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]; //define array
 function addElementToBeginningOfArray(array, element) {
-  array.unshift(element)
-  console.log(element);
+var array = [array, ...chocolateBars]
+return array
 };
 function destructivelyAddElementToBeginningOfArray(array, element) {
-
+return array.push(element)
 }
