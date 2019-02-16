2a8014d3fdcbbd5e082fc3d7e641dd189c3417e0
sensible-state-8093 <reilly.hartigan@yale.edu>
2019-02-16 19:56:43 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,1 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]; //define array
+function addElementToEndOfArray() {
+
+};
+function destructivelyAddElementToBeginningOfArray() {
+
+}
