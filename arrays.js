ae78d525786d06b4c13c24f4513424eb006b0a0e
sensible-state-8093 <reilly.hartigan@yale.edu>
2019-02-16 20:19:18 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]; //define array
 function addElementToBeginningOfArray(array, element) {
 return [element, ...array]
+return array
 };
 function destructivelyAddElementToBeginningOfArray(array, element) {
 return array.onshift(element)
